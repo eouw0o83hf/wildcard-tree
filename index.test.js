7490e7644aa93646a3a29dfeaea1628b4d11f8e7
Nathan Landis <nathanlandis@gmail.com>
2019-11-28 15:27:46 -0600

    Test passing commit

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 import sut from './index';
 
 test('should pass', () => {
-  const result = sut('test');
-  expect(result).toEqual('test');
+  const result = sut('test 2');
+  expect(result).toEqual('test 2');
 });
